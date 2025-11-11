55798a8e691902d840c8d3ae342cf48b5f6f7a54
PedroSilva01 <pedro.kzn.victor@gmail.com>
2025-11-10 22:35:10 -0300

    fix: resolve Vercel 404 on valid routes

---
+++ dist/assets/utils-DLlLIogE.js
@@ -0,0 +1,1 @@
+import{A as t,d as c}from"./main--zAWDl51.js";function n(...r){return t(c(r))}export{n as c};
--- dist/assets/utils-SArqjpXE.js
+++
@@ -1,1 +0,0 @@
-import{A as t,d as c}from"./main-CulyknMy.js";function n(...r){return t(c(r))}export{n as c};
